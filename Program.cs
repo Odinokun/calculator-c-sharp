3b04cec54be8f53d5c78de77e4e850f69882d557
odinokun <odinokun@gmail.com>
2020-09-17 18:47:48 +0300

    start

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите первое значение");
+            string inputValStr = Console.ReadLine();
+            int a = int.Parse(inputValStr);
+
+            Console.WriteLine("Введите оператор");
+            inputValStr = Console.ReadLine();
+            char oper = char.Parse(inputValStr);
+
+            Console.WriteLine("Введите второе значение");
+            inputValStr = Console.ReadLine();
+            int b = int.Parse(inputValStr);
+
+            Console.WriteLine(a + oper + b);
+
+            //if (oper == "+") 
+            //{
+            //    Console.WriteLine(a + b);
+            //}
+            //else if (oper == "-")
+            //{
+            //    Console.WriteLine(a - b);
+            //}
+            //else if (oper == "*")
+            //{
+            //    Console.WriteLine(a * b);
+            //}
+            //else if (oper == "/")
+            //{
+            //    Console.WriteLine(a / b);
+            //} else
+            //{
+            //    Console.WriteLine("Введите корректные данные.");
+            //}
+
+        }
+    }
+}
